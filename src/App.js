8f81a34a8bc42bb21e98ae223ded9e8565928d0f
PAS240886 <popov.a.connect@gmail.com>
2022-10-20 17:20:51 +0300

    Fetch

--- src/App.js
+++ src/App.js
@@ -7,25 +7,18 @@ class App extends Component {
     super();
 
     this.state = {
-    monsters:[
-      {
-        name:"Linda",
-        id: "1111",
-      },
-      {
-        name:"Frank",
-        id: "1112",
-      },
-      {
-        name: "Jacky",
-        id: "1113",
-      },
-      {
-        name: "Less",
-        id: "1114",
-      },
-    ]
-    }
+    monsters:[]
+    };
+  }
+
+  componentDidMount (){
+    fetch ('https://jsonplaceholder.typicode.com/users')
+      .then ((response) => response.json())
+      .then ((users) => this.setState(
+        () => {
+        return {monsters: users}
+      }) 
+      );
   }
   render() {
     return (
